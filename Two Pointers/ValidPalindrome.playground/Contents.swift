13210ace7e7c6eef956644ba9186751e9a992c69
Beyza Zengin <112340720+BeyzaZngn@users.noreply.github.com>
2025-10-10 16:07:03 +0300

    completed.

--- Two Pointers/ValidPalindrome.playground/Contents.swift
+++ Two Pointers/ValidPalindrome.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func isPalindrome(_ s: String) -> Bool {
+    
+    var word : String = ""
+    for i in s {
+        if i.isLetter || i.isNumber {
+            word += String(i).lowercased()
+        }
+    }
+    return word == String(word.reversed())
+}
+print(isPalindrome("A man, a plan, a canal: Panama"))
+
+// İkinci Yol
+
+ func isPalindromeSecond(_ s: String) -> Bool {
+     
+     let filtered = s.lowercased().filter{$0.isLetter || $0.isNumber}
+     return filtered == String(filtered.reversed())
+ 
+ }
+ print(isPalindromeSecond("A man, a plan, a canal: Panama"))
